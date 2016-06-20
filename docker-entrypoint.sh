7ddb014874dce92b3ebb2c9999a1c30686132b1d
David Numan <david.numan@civicactions.com>
2016-06-20 11:20:26 -0400

    Adding the -o to force the groupmod command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 # Getting file permissions on host for docker socket
 if [ -e /var/run/docker.sock ]; then
   DOCKER_GID_ON_HOST=$(stat -c %g /var/run/docker.sock)
-  sudo groupmod -g $DOCKER_GID_ON_HOST docker
+  # Set the docker group id to match host so Jenkins user also has access.
+  sudo groupmod -og $DOCKER_GID_ON_HOST docker
 else
   echo -e "\nThe host docker.sock is not mounted in the container"
   echo -e "To give jenkins access to the docker host, run this image with '-v /var/run/docker.sock:/var/run/docker.sock'\n"
